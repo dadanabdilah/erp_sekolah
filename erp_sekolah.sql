7bbd3da953de725a867a75859ec25f4de3a221c8
unknown <abdilahdadan5@gmail.com>
2023-06-18 15:14:45 +0700

    first commit

--- erp_sekolah.sql
+++ erp_sekolah.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 15, 2023 at 02:06 PM
+-- Generation Time: Jun 18, 2023 at 08:13 AM
 -- Server version: 8.0.30
 -- PHP Version: 7.4.33
 
@@ -37,7 +37,8 @@ CREATE TABLE `kategori` (
 --
 
 INSERT INTO `kategori` (`id`, `kategori`) VALUES
-(1, 'Pemasukan');
+(1, 'Pemasukan'),
+(2, 'Belanja');
 
 -- --------------------------------------------------------
 
@@ -55,7 +56,8 @@ CREATE TABLE `kelas` (
 --
 
 INSERT INTO `kelas` (`id`, `kelas`) VALUES
-(3, '10 A 2023');
+(3, '10 A 2023'),
+(4, '10 B 2023');
 
 -- --------------------------------------------------------
 
@@ -77,7 +79,8 @@ CREATE TABLE `keuangan` (
 --
 
 INSERT INTO `keuangan` (`id`, `nominal`, `jenis`, `id_kategori`, `keterangan`, `tanggal`) VALUES
-(1, 10000, 'Uang Masuk', 1, 'fghj', '2023-06-15');
+(1, 10000, 'Uang Masuk', 1, 'fghj', '2023-06-15'),
+(3, 10000000, 'Uang Keluar', 2, 'Belanja  Barang', '2023-06-15');
 
 -- --------------------------------------------------------
 
@@ -87,8 +90,16 @@ INSERT INTO `keuangan` (`id`, `nominal`, `jenis`, `id_kategori`, `keterangan`, `
 
 CREATE TABLE `mapel` (
   `id` int NOT NULL,
-  `mapel` varchar(50) CHARACTER SET utf8mb4  NOT NULL
+  `mapel` varchar(50) CHARACTER SET utf8mb4 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mapel`
+--
+
+INSERT INTO `mapel` (`id`, `mapel`) VALUES
+(2, 'IPA'),
+(3, 'IPS');
 
 -- --------------------------------------------------------
 
@@ -103,6 +114,15 @@ CREATE TABLE `nilai` (
   `id_mapel` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `nilai`
+--
+
+INSERT INTO `nilai` (`id`, `nilai`, `nis`, `id_mapel`) VALUES
+(2, 10, 21212, 2),
+(3, 8, 21212, 3),
+(4, 10, 3234090, 3);
+
 -- --------------------------------------------------------
 
 --
@@ -114,7 +134,7 @@ CREATE TABLE `pengguna` (
   `nama_pengguna` varchar(128) NOT NULL,
   `email` varchar(128) NOT NULL,
   `password` varchar(128) NOT NULL,
-  `jabatan` enum('Admin','Kepala_Sekolah','Keuangan','Wali_Kelas') CHARACTER SET utf8mb4  NOT NULL
+  `jabatan` enum('Admin','Kepala_Sekolah','Keuangan','Wali_Kelas') CHARACTER SET utf8mb4 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -122,8 +142,10 @@ CREATE TABLE `pengguna` (
 --
 
 INSERT INTO `pengguna` (`id`, `nama_pengguna`, `email`, `password`, `jabatan`) VALUES
-(2, 'Dadan Abdilah', 'dadan@gmail.com', '25f9e794323b453885f5181f1b624d0b', 'Admin'),
-(3, 'Dadan', 'dadana@gmail.com', '25f9e794323b453885f5181f1b624d0b', 'Keuangan');
+(2, 'Dadan Abdilah', 'admin@gmail.com', '25f9e794323b453885f5181f1b624d0b', 'Admin'),
+(3, 'Dadan', 'keuangan@gmail.com', '25f9e794323b453885f5181f1b624d0b', 'Keuangan'),
+(4, 'Wali Kelas', 'walikelas@gmail.com', '25f9e794323b453885f5181f1b624d0b', 'Wali_Kelas'),
+(5, 'Kepala Sekolah', 'kepsek@gmail.com', '25f9e794323b453885f5181f1b624d0b', 'Kepala_Sekolah');
 
 -- --------------------------------------------------------
 
@@ -134,6 +156,7 @@ INSERT INTO `pengguna` (`id`, `nama_pengguna`, `email`, `password`, `jabatan`) V
 CREATE TABLE `siswa` (
   `nis` varchar(16) NOT NULL,
   `id_kelas` int NOT NULL,
+  `id_thn_akademik` int NOT NULL,
   `nama_siswa` varchar(128) NOT NULL,
   `jk` enum('L','P') NOT NULL,
   `alamat` mediumtext NOT NULL,
@@ -146,8 +169,29 @@ CREATE TABLE `siswa` (
 -- Dumping data for table `siswa`
 --
 
-INSERT INTO `siswa` (`nis`, `id_kelas`, `nama_siswa`, `jk`, `alamat`, `no_hp`, `nama_orang_tua`, `pekerjaan_orang_tua`) VALUES
-('21212', 3, 'Dadan Abdilah', 'L', 'sqq', '12121', '21212', '212');
+INSERT INTO `siswa` (`nis`, `id_kelas`, `id_thn_akademik`, `nama_siswa`, `jk`, `alamat`, `no_hp`, `nama_orang_tua`, `pekerjaan_orang_tua`) VALUES
+('16526', 3, 2, 'Testing', 'L', 'Kuningan', '0976543567890', 'Test', 'Test'),
+('21212', 3, 2, 'Dadan Abdilah', 'L', 'Test', '0876546789', 'Test', 'Test'),
+('3234090', 4, 2, 'Ahmad', 'L', 'Test', '0856546789', 'Test', 'Test');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tahun_akademik`
+--
+
+CREATE TABLE `tahun_akademik` (
+  `id` int NOT NULL,
+  `tahun` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tahun_akademik`
+--
+
+INSERT INTO `tahun_akademik` (`id`, `tahun`) VALUES
+(2, '2023'),
+(3, '2024');
 
 --
 -- Indexes for dumped tables
@@ -199,6 +243,12 @@ ALTER TABLE `siswa`
   ADD KEY `id_kelas` (`id_kelas`);
 
 --
+-- Indexes for table `tahun_akademik`
+--
+ALTER TABLE `tahun_akademik`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -206,36 +256,42 @@ ALTER TABLE `siswa`
 -- AUTO_INCREMENT for table `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `kelas`
 --
 ALTER TABLE `kelas`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `keuangan`
 --
 ALTER TABLE `keuangan`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `mapel`
 --
 ALTER TABLE `mapel`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `nilai`
 --
 ALTER TABLE `nilai`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `pengguna`
 --
 ALTER TABLE `pengguna`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tahun_akademik`
+--
+ALTER TABLE `tahun_akademik`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
